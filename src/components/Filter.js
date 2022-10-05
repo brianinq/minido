aae0311d29705f68d27a819e7f816c890254cd24
brian <kipkoechbrian0@gmail.com>
2022-10-05 23:07:36 +0300

    add filter functionality on todos

---
+++ src/components/Filter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Filter({ category, onCategoryChange }) {
+  return (
+    <div className="Filter">
+      <select name="filter" value={category} onChange={(e) => onCategoryChange(e.target.value)}>
+        <option value="All">Filter by category</option>
+        <option value="General">General</option>
+        <option value="Code">Code</option>
+        <option value="Leisure">Leisure</option>
+        <option value="Family">Family</option>
+      </select>
+    </div>
+  );
+}
